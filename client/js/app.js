ecc4a7b0fd9c1927d12f77a9b7120e5e140ac0af
Cybergatenet <abelchinedu2@gmail.com>
2020-12-20 20:01:03 +0100

    getting started with this templete project

---
+++ client/js/app.js
@@ -0,0 +1,37 @@
+// Scroll to top
+const scrolltp = document.querySelector("#scrollToTop");
+scrolltp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 700) {
+    scrolltp.style.opacity = 1;
+  } else {
+    scrolltp.style.opacity = 0;
+  }
+});
+
+// Theme
+
+const themeToggle = document.querySelector(".checkbox");
+const body = document.querySelector("body");
+const darkmode = localStorage.getItem("dark");
+
+if (darkmode) {
+  body.classList.add("dark");
+  themeToggle.checked = true;
+}
+
+themeToggle.addEventListener("change", () => {
+  body.classList.toggle("dark");
+
+  if (body.classList.contains("dark")) {
+    localStorage.setItem("dark", "active");
+  } else {
+    localStorage.removeItem("dark");
+  }
+});
